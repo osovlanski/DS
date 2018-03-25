36c5721b29e71c4a977cca496e6225b4f7681cba
Itay Osovlanski <itayosov@gmail.com>
2018-03-26 01:02:10 +0300

    Initial Commit

--- assignments/src/com/bgu/workground/assignment2/MemoryManagement/BTree/BTIteratorIF.java
+++ assignments/src/com/bgu/workground/assignment2/MemoryManagement/BTree/BTIteratorIF.java
@@ -4,12 +4,16 @@ package com.bgu.workground.assignment2.MemoryManagement.BTree;
 
 /**
  * Interface BTIteratorIF
- * @author tnguyen
+ * @author itay
  */
 public interface BTIteratorIF <K extends Comparable, V> {
     public boolean item(K key, V value);
     public BTNode<K, V> getRootNode();
     public long size();
     public void clear();
+    public BTree insert(K key, V value);
+    public V delete(K key);
+    public V read(K key);
+    public BTree write(K key, V value);
 
 }
